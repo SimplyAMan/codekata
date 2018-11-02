2dea041447a01aff7663a757488c59734938ec59
= <=>
2018-11-02 21:02:49 +0200

    First version of iterator implementations

--- src/Kata2.java
+++ src/Kata2.java
@@ -1,3 +1,40 @@
 public class Kata2 {
+    private boolean primaryCheck(int value, int[] sortedArray) {
+        if (sortedArray.length == 0
+                || sortedArray[0] > value
+                || sortedArray[sortedArray.length - 1] < value) {
+            return true;
+        }
+        return false;
+    }
 
+    public int iterator_chop(int value, int[] sortedArray) {
+        int result = -1;
+        if (!primaryCheck(value, sortedArray)) {
+            int minIndex = 0;
+            int maxIndex = sortedArray.length;
+            for (int i = 0; i < sortedArray.length; i++) {
+                int index = minIndex + (maxIndex - minIndex) / 2;
+                if (sortedArray[index] == value) {
+                    result = index;
+                } else if (value < sortedArray[index]) {
+                    if (maxIndex != index) {
+                        maxIndex = index;
+                    } else {
+                        maxIndex = index - 1;
+                    }
+                } else if (sortedArray[index] < value) {
+                    if (minIndex != index) {
+                        minIndex = index;
+                    } else {
+                        minIndex = index + 1;
+                    }
+
+                } else {
+                    result = -1;
+                }
+            }
+        }
+        return result;
+    }
 }

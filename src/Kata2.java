99fbc69f8a8976e8985e83e0e8cd9273bb88aa8d
= <=>
2018-11-03 20:53:39 +0200

    Recursive implementation

--- src/Kata2.java
+++ src/Kata2.java
@@ -1,13 +1,13 @@
 public class Kata2 {
-    private boolean primaryCheck(int value, int[] sortedArray) {
-        return  (sortedArray.length == 0
+    private boolean checkIfValueIsInArray(int value, int[] sortedArray) {
+        return  !(sortedArray.length == 0
                 || sortedArray[0] > value
                 || sortedArray[sortedArray.length - 1] < value);
     }
 
     public int iterator_chop(int value, int[] sortedArray) {
         int result = -1;
-        if (!primaryCheck(value, sortedArray)) {
+        if (checkIfValueIsInArray(value, sortedArray)) {
             int minIndex = 0;
             int maxIndex = sortedArray.length;
             for (int i = 0; i < sortedArray.length/2 + 1; i++) {
@@ -18,6 +18,29 @@ public class Kata2 {
                     maxIndex = maxIndex != index ? index : index - 1;
                 } else if (sortedArray[index] < value) {
                     minIndex = minIndex != index ? index : index + 1;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int recursive_chop(int value, int[] sortedArray) {
+        int result = -1;
+        if(checkIfValueIsInArray(value, sortedArray)) {
+            int middleIndex = sortedArray.length/2;
+            if (value == sortedArray[middleIndex]) {
+                result = middleIndex;
+            } else if (sortedArray.length > 1 ) {
+                if (value < sortedArray[middleIndex]) {
+                    int[] newArray = new int[middleIndex];
+                    System.arraycopy(sortedArray, 0, newArray, 0, middleIndex);
+                    result = recursive_chop(value, newArray);
+                } else if (value > sortedArray[middleIndex]) {
+                    int length = sortedArray.length - middleIndex - 1;
+                    int[] newArray = new int[length];
+                    System.arraycopy(sortedArray, middleIndex + 1, newArray, 0, length);
+                    int tempResult = recursive_chop(value, newArray);
+                    result = tempResult == -1 ? -1 : middleIndex + tempResult + 1;
                 }
             }
         }
--- test/Kata2Test.java
+++ test/Kata2Test.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class Kata2Test {
     private Kata2 kata = new Kata2();
     @Test
-    public void testChop() {
+    public void testIteratorChop() {
         assertEquals(-1, kata.iterator_chop(3, new int[]{}));
         assertEquals(-1, kata.iterator_chop(3, new int[]{1}));
         assertEquals(0,  kata.iterator_chop(1, new int[]{1}));
@@ -28,4 +28,30 @@ public class Kata2Test {
         assertEquals(-1, kata.iterator_chop(6, new int[]{1, 3, 5, 7}));
         assertEquals(-1, kata.iterator_chop(8, new int[]{1, 3, 5, 7}));
     }
+    @Test
+    public void testRecursiveChop() {
+        assertEquals(-1, kata.recursive_chop(3, new int[]{}));
+        assertEquals(-1, kata.recursive_chop(3, new int[]{1}));
+        assertEquals(0,  kata.recursive_chop(1, new int[]{1}));
+        //------------------------
+        assertEquals(0,  kata.recursive_chop(1, new int[]{1, 3, 5}));
+        assertEquals(1,  kata.recursive_chop(3, new int[]{1, 3, 5}));
+        assertEquals(2,  kata.recursive_chop(5, new int[]{1, 3, 5}));
+        assertEquals(-1, kata.recursive_chop(0, new int[]{1, 3, 5}));
+        assertEquals(-1, kata.recursive_chop(2, new int[]{1, 3, 5}));
+        assertEquals(-1, kata.recursive_chop(4, new int[]{1, 3, 5}));
+        assertEquals(-1, kata.recursive_chop(6, new int[]{1, 3, 5}));
+        //------------------------
+        assertEquals(0,  kata.recursive_chop(1, new int[]{1, 3, 5, 7}));
+        assertEquals(1,  kata.recursive_chop(3, new int[]{1, 3, 5, 7}));
+        assertEquals(2,  kata.recursive_chop(5, new int[]{1, 3, 5, 7}));
+        assertEquals(3,  kata.recursive_chop(7, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, kata.recursive_chop(0, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, kata.recursive_chop(2, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, kata.recursive_chop(4, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, kata.recursive_chop(6, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, kata.recursive_chop(8, new int[]{1, 3, 5, 7}));
+    }
+
+
 }

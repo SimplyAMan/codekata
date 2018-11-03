a6d9098bcfc240acfa96e6362e5a2923f312d937
= <=>
2018-11-03 20:33:53 +0200

    Refactoring

--- src/Kata2.java
+++ src/Kata2.java
@@ -1,11 +1,8 @@
 public class Kata2 {
     private boolean primaryCheck(int value, int[] sortedArray) {
-        if (sortedArray.length == 0
+        return  (sortedArray.length == 0
                 || sortedArray[0] > value
-                || sortedArray[sortedArray.length - 1] < value) {
-            return true;
-        }
-        return false;
+                || sortedArray[sortedArray.length - 1] < value);
     }
 
     public int iterator_chop(int value, int[] sortedArray) {
@@ -13,25 +10,14 @@ public class Kata2 {
         if (!primaryCheck(value, sortedArray)) {
             int minIndex = 0;
             int maxIndex = sortedArray.length;
-            for (int i = 0; i < sortedArray.length; i++) {
+            for (int i = 0; i < sortedArray.length/2 + 1; i++) {
                 int index = minIndex + (maxIndex - minIndex) / 2;
                 if (sortedArray[index] == value) {
                     result = index;
                 } else if (value < sortedArray[index]) {
-                    if (maxIndex != index) {
-                        maxIndex = index;
-                    } else {
-                        maxIndex = index - 1;
-                    }
+                    maxIndex = maxIndex != index ? index : index - 1;
                 } else if (sortedArray[index] < value) {
-                    if (minIndex != index) {
-                        minIndex = index;
-                    } else {
-                        minIndex = index + 1;
-                    }
-
-                } else {
-                    result = -1;
+                    minIndex = minIndex != index ? index : index + 1;
                 }
             }
         }
